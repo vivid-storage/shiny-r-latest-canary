c0a5bd8ae6d38cbec43e01f0218c757f19fa68b9
vivid-storage <165187161+vivid-storage@users.noreply.github.com>
2026-04-23 10:42:27 -0700

    Update app.R

--- app.R
+++ app.R
@@ -1,18 +1,27 @@
-library(shiny)
-  library(Rcpp)
-  library(ggplot2)
-  library(dplyr)
-  library(tidyr)
+library(shiny)                                                                                                                                                                   
+  library(Rcpp)                                                                                                                                                                    
+  library(ggplot2)                                                                                                                                                                 
+  library(dplyr)                                                                                                                                                                   
+  library(tidyr)                                            
   library(DT)
   library(plotly)
   library(jsonlite)
   library(fs)
+                                                                                                                                                                                   
+  PACKAGES <- c("shiny", "Rcpp", "ggplot2", "dplyr", "tidyr", "DT", "plotly", "jsonlite", "fs")                                                                                    
+  versions <- setNames(                                                                                                                                                            
+    lapply(PACKAGES, function(p) as.character(packageVersion(p))),                                                                                                                 
+    PACKAGES                                                
+  )
 
   ui <- fluidPage(
     titlePanel("Connect Cloud Canary"),
-    p("If you can see this, all canary packages loaded successfully.")
+    tags$pre(                                                                                                                                                                      
+      id = "versions",
+      jsonlite::toJSON(versions, auto_unbox = TRUE, pretty = TRUE)                                                                                                                 
+    )                                                                                                                                                                              
   )
-
-  server <- function(input, output, session) {}
+                                                                                                                                                                                   
+  server <- function(input, output, session) {}             
 
   shinyApp(ui, server)
